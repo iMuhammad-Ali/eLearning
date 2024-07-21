4f2977a191a0a5eefea5f6da83040c5d0c3229c5
Muhammad Ali <muhammad_ali@nextpak.org>
2024-07-21 14:23:24 +0500

    first commit

---
+++ config/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEuromAy8nAaaGV61iXioTnMZEqkiET7g",
+  authDomain: "elearning-9b8a0.firebaseapp.com",
+  projectId: "elearning-9b8a0",
+  storageBucket: "elearning-9b8a0.appspot.com",
+  messagingSenderId: "537561500856",
+  appId: "1:537561500856:web:42593e9e79b7538df1384d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
